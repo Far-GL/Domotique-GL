be20c478fc14844c297840575e13b1dd4be13391
sarkou <58863062+sarkou@users.noreply.github.com>
2020-02-11 15:47:35 +0100

    Update Ordre.java

--- src_domotique/Ordre.java
+++ src_domotique/Ordre.java
@@ -1,5 +1,33 @@
 package domotique;
 
 public class Ordre {
-
+  private String commande;
+  private HashMap<motsclé,ordre>
+    
+    
+   public void receptionOrdre void (String commande){
+      this.commande=commande;	
+	}
+  public String eteindre(){
+    if(ordre.equals(on)){
+    
+  }
+  public String allumer(){
+  
+  }
+  public String eteindre(){
+    if(ordre.equals(off)){
+     
+  }
+  public String ouvrir(){
+    if(ordre.equals(open)){
+    
+  }
+   public String fermer(){
+    if(ordre.equals()){
+    
+  }
+  
+  
+  
 }

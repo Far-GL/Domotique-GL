a795729d98318667198c11b9ebe17b88ec93cfd2
sarkou <58863062+sarkou@users.noreply.github.com>
2020-02-11 14:16:44 +0100

    Update UniteCentrale.java

--- src_domotique/UniteCentrale.java
+++ src_domotique/UniteCentrale.java
@@ -1,5 +1,11 @@
 package domotique;
 
 public class UniteCentrale {
-
+  private salon Salon;
+	private chambre Chambre;
+	private cuisine Cuisine;
+	private salleDeBain SalleDeBain;
+	private entree Entree;
+	private couloir Couloir;
+	
 }

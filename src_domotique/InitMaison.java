7fa0fce14b1fde9faef7f759a358429f2e1d500b
sarkou <58863062+sarkou@users.noreply.github.com>
2020-02-12 12:25:18 +0100

    Update InitMaison.java

--- src_domotique/InitMaison.java
+++ src_domotique/InitMaison.java
@@ -1,6 +1,6 @@
 package domotique;
 
-public class InitMaison {
+public class InitMaison {  // ici si tu cree des objets alors il faut creer la "main " je crois ou je trompe
 	Chambre chambre = new Chambre();
 	Couloir couloir = new Couloir();
 	Cuisine cuisine = new Cuisine();
